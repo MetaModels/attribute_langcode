0c397e75ae44df3c56519049218a9e2017dfb10f
Mini Model <minimodel@metamodel.me>
2019-03-05 19:33:33 +0100

    Update PHPDoc file headers

--- tests/DependencyInjection/MetaModelsAttributeLangCodeExtensionTest.php
+++ tests/DependencyInjection/MetaModelsAttributeLangCodeExtensionTest.php
@@ -1,21 +1,20 @@
 <?php
 
 /**
- * * This file is part of MetaModels/attribute_langcode.
+ * This file is part of MetaModels/attribute_langcode.
  *
- * (c) 2012-2017 The MetaModels team.
+ * (c) 2012-2019 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
  * This project is provided in good faith and hope to be usable by anyone.
  *
- * @package    MetaModels
- * @subpackage AttributeLangCode
+ * @package    MetaModels/attribute_langcode
  * @author     David Molineus <david.molineus@netzmacht.de>
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
- * @copyright  2012-2017 The MetaModels team.
- * @license    https://github.com/MetaModels/attribute_text/blob/master/LICENSE LGPL-3.0
+ * @copyright  2012-2019 The MetaModels team.
+ * @license    https://github.com/MetaModels/attribute_langcode/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
 
