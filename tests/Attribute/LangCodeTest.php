e64a25f8e4a2439750de1c198c49b5621b9f6d4f
Ingolf Steinhardt <info@e-spin.de>
2021-08-01 18:10:48 +0200

    Switch to github-actions

--- tests/Attribute/LangCodeTest.php
+++ tests/Attribute/LangCodeTest.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/attribute_langcode.
  *
- * (c) 2012-2019 The MetaModels team.
+ * (c) 2012-2021 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -14,7 +14,8 @@
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @author     Sven Baumann <baumann.sv@gmail.com>
  * @author     David Molineus <david.molineus@netzmacht.de>
- * @copyright  2012-2019 The MetaModels team.
+ * @author     Ingolf Steinhardt <info@e-spin.de>
+ * @copyright  2012-2021 The MetaModels team.
  * @license    https://github.com/MetaModels/attribute_langcode/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
@@ -29,6 +30,8 @@ use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 /**
  * Unit tests to test class Decimal.
+ *
+ * @covers \MetaModels\AttributeLangCodeBundle\Attribute\LangCode
  */
 class LangCodeTest extends TestCase
 {
